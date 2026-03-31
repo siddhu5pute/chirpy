c6491806adb5b9c30de28cdafdc12b218dde6062
siddhu5pute <5putesidd1305@gmail.com>
2026-03-31 18:16:34 +0530

    feat: add user creation and chirp association with user ID

--- handler.go
+++ handler.go
@@ -1,11 +1,63 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+	"strings"
+	"errors"
 )
+
 
 func handlerOne(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Problem while marshaling: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type resError struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, resError{Error: msg})
+}
+
+func validateChirp(body string) (string, error){
+	if len(body) > 140 {
+    return "", errors.New("Chirp is too long")
+	}
+	cleaned := profanCheck(body)
+	return cleaned, nil
+}
+
+func profanCheck(msg string) string {
+	badWords := map[string]bool{
+		"kerfuffle": true,
+		"sharbert":  true,
+		"fornax":    true,
+	}
+
+	words := strings.Fields(msg)
+
+	for i, word := range words {
+		wordLower := strings.ToLower(word)
+		if badWords[wordLower] {
+			words[i] = "****"
+		}
+	}
+
+	return strings.Join(words, " ")
 }
--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
+// Postgres driver (needed for database/sql to talk to Postgres)
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/siddhu5pute/chirpy/internal/database"
+
+	"time"
+
+	"github.com/google/uuid"
 )
 
+// Load .env file
+
+// IMPORT YOUR SQLC GENERATED PACKAGE
+
+// apiConfig now ALSO holds DB queries
 type apiConfig struct {
 	fileserverHits atomic.Int32
+
+	// NEW: SQLC queries object
+	dbQueries *database.Queries
+	platform  string
 }
 
+// Middleware to count hits
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
@@ -18,22 +40,145 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
+// Show metrics in browser
 func (cfg *apiConfig) handlerReqCount(w http.ResponseWriter, _ *http.Request) {
-	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(fmt.Sprintf(`
+	<html>
+		<body>
+    		<h1>Welcome, Chirpy Admin</h1>
+    		<p>Chirpy has been visited %d times!</p>
+  		</body>
+	</html>`, cfg.fileserverHits.Load())))
 }
 
-func (cfg *apiConfig) handlerResetCount(w http.ResponseWriter, _ *http.Request) {
+// Reset counter
+func (cfg *apiConfig) handlerResetCount(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		respondWithError(w, 403, "Forbidden")
+		return
+	}
+	err := cfg.dbQueries.DeleteAllUsers(r.Context())
+	if err != nil {
+		respondWithError(w, 500, "Internal Server Error")
+		return
+	}
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(200)
 }
 
+//Create User
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+func (cfg *apiConfig) handlercreateUser(w http.ResponseWriter, r *http.Request) {
+	r_email := struct {
+		Email string `json:"email"`
+	}{}
+	err := json.NewDecoder(r.Body).Decode(&r_email)
+	if err != nil {
+		respondWithError(w, 500, "Couldn't decode parameters")
+		return
+	}
+
+	user, err := cfg.dbQueries.CreateUser(r.Context(), r_email.Email)
+	if err != nil {
+		respondWithError(w, 500, "Couldn't create user")
+		return
+	}
+
+	respondWithJSON(w, 201, User{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	})
+}
+
+//Create CHIRP 
+type Chirp struct {
+	ID uuid.UUID `json:"id"`
+    CreatedAt time.Time `json:"created_at"`
+    UpdatedAt time.Time `json:"updated_at"`
+	Body string `json:"body"`
+	UserID uuid.UUID `json:"user_id"`
+}
+
+func (cfg *apiConfig) handlercreateChirp(w http.ResponseWriter, r *http.Request) {
+	c_body := struct{
+		Body string `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
+	}{}
+	err := json.NewDecoder(r.Body).Decode(&c_body)
+	if err != nil {
+		respondWithError(w, 500, "Couldn't decode parameters")
+		return 
+	}
+	validatedB, err := validateChirp(c_body.Body)
+	if err != nil {
+		respondWithError(w, 400, err.Error())
+		return 
+	}
+
+	chirp, err := cfg.dbQueries.CreateChirp(r.Context(), database.CreateChirpParams{
+		Body: validatedB,
+		UserID: c_body.UserID,
+	})
+	if err != nil {
+		respondWithError(w, 500, "Couldn't create chirp")
+		return 
+	}
+	respondWithJSON(w, 201, Chirp{
+		ID: chirp.ID,
+		CreatedAt: chirp.CreatedAt,
+		UpdatedAt: chirp.UpdatedAt,
+		Body: chirp.Body,
+		UserID: chirp.UserID,
+	})
+
+}
 func main() {
-	apiConf := apiConfig{}
+
+	// ✅ STEP 1: Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
+	}
+
+	// ✅ STEP 2: Get DB URL from environment
+	dbURL := os.Getenv("DB_URL")
+	dbPLATFORM := os.Getenv("PLATFORM")
+
+	// ✅ STEP 3: Open DB connection
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Cannot connect to DB:", err)
+	}
+
+	// ✅ STEP 4: Create SQLC Queries instance
+	dbQueries := database.New(db)
+
+	// ✅ STEP 5: Inject into apiConfig
+	apiConf := apiConfig{
+		dbQueries: dbQueries,
+		platform:  dbPLATFORM,
+	}
+
+	// ROUTES
 	mux := http.NewServeMux()
+
 	mux.Handle("/app/", apiConf.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("GET /healthz", handlerOne)
-	mux.HandleFunc("GET /metrics", apiConf.handlerReqCount)
-	mux.HandleFunc("POST /reset", apiConf.handlerResetCount)
+
+	mux.HandleFunc("GET /api/healthz", handlerOne)
+	mux.HandleFunc("GET /admin/metrics", apiConf.handlerReqCount)
+	mux.HandleFunc("POST /admin/reset", apiConf.handlerResetCount)
+	mux.HandleFunc("POST /api/chirps", apiConf.handlercreateChirp)
+	mux.HandleFunc("POST /api/users", apiConf.handlercreateUser)
 
 	server := &http.Server{
 		Addr:    ":8080",
